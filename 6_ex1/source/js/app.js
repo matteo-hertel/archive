b30fa0739176771accfedb22689dd746a6219b1c
Matteo Hertel <info@matteohertel.com>
2016-01-10 11:14:21 +0000

    Lesson 6 Subscription Plans Exercise complete

--- 6_ex1/source/js/app.js
+++ 6_ex1/source/js/app.js
@@ -2,6 +2,35 @@ new Vue({
 
     el: '#demo',
     data: {
-        heading: "Hello"
+        plans: [{
+            name: "Enterprise",
+            price: 100
+        }, {
+            name: "Pro",
+            price: 50
+        }, {
+            name: "Personal",
+            price: 10
+        }, {
+            name: "Free",
+            price: 0
+        }],
+        active: {}
+    },
+    components: {
+        plan: {
+            template: "#planTempalte",
+            props: ["plan", "active"],
+            methods: {
+                setActivePlan: function() {
+                    this.active = this.plan;
+                }
+            },
+            computed: {
+                isUpgrade: function() {
+                    return this.plan.price > this.active.price;
+                }
+            }
+        }
     }
 });
