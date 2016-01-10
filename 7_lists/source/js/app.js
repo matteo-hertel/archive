502f364235c31376975c4f365d9bf2a925b272d6
Matteo Hertel <info@matteohertel.com>
2016-01-10 15:16:42 +0000

    Lesson 7 lists complete

---
+++ 7_lists/source/js/app.js
@@ -0,0 +1,24 @@
+new Vue({
+
+    el: '#demo',
+    data: {
+        tasks: [{
+            body: "Go to the store",
+            completed: false
+        }, {
+            body: "Go to the bank",
+            completed: false
+        }, {
+            body: "Go to the doctor",
+            completed: true
+        }, {
+            body: "Go to to work",
+            completed: false
+        }]
+    },
+    methods: {
+        toggleComplete: function(task) {
+            task.completed = !task.completed;
+        }
+    }
+});
