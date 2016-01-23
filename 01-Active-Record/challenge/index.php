870eaaf102a770944760c27b2f1f081d7a9126da
Matteo Hertel <info@matteohertel.com>
2016-01-23 23:46:00 +0000

    Lesson 1 Active Records complete

--- 01-Active-Record/challenge/index.php
+++ 01-Active-Record/challenge/index.php
@@ -8,8 +8,16 @@
  *   videos. It will make way more sense and be more useful in general.
  */
 
+include_once __DIR__ . '/vendor/autoload.php';
 include_once 'posts.php';
 
-$parameters = array('id' => null, 'title' => 'Sample Title', 'body' => 'This is the body', 'create_date' => '2015-01-27');
+echo"<pre style='color:#59E817; background-color:black; word-wrap:break-word;'>";
+var_dump(Post::first());
+echo"</pre>";
 
-Post::create($parameters);
+// $post = new Post();
+//
+// $post->title = 'Sample Title';
+// $post->body = 'This is the body';
+//
+// $post->save();
