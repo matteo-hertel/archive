8a31406e2043b3237f55f256688809f2ad80f23a
Matteo Hertel <info@matteohertel.com>
2016-08-01 14:48:13 +0100

    add script to change commit email

--- test.bash
+++
@@ -1,3 +0,0 @@
-git filter-branch --commit-filter 'if [ "$GIT_AUTHOR_EMAIL" = "matteo@feastcreative.com" ];
-  then export GIT_AUTHOR_NAME="Matteo Hertel"; export GIT_AUTHOR_EMAIL=info@matteohertel.com;
-    fi; git commit-tree "$@"'
